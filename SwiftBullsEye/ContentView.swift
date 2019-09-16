7bf0a294100e5f7562e5c26bc8e18d9474e193b8
Marvin <marvkipi@gmail.com>
2019-09-15 22:36:46 -0500

    init

---
+++ SwiftBullsEye/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftBullsEye
+//
+//  Created by Marvin Nikijuluw on 9/15/19.
+//  Copyright © 2019 Marvin Nikijuluw. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
