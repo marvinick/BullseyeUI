8f58052f80fadd9443987d8cbc8ad0d0e06cee9c
Marvin <marvkipi@gmail.com>
2019-09-16 21:40:34 -0500

    alert button

--- SwiftBullsEye/ContentView.swift
+++ SwiftBullsEye/ContentView.swift
@@ -9,8 +9,46 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var alertIsVisible: Bool = false
+    @State var whoIsVisible: Bool = false
+    
     var body: some View {
-        Text("Hello World")
+        HStack {
+            VStack {
+                Text("Welcome to my first app")
+                    .fontWeight(.semibold)
+                    .foregroundColor(Color.green)
+                Button(action: {
+                    print("Button pressed")
+                    self.alertIsVisible = true
+                }) {
+                    Text("Hit me")
+                }
+                .alert(isPresented: $alertIsVisible) { () ->
+                    Alert in
+                    return Alert(title: Text("Hello there!"), message: Text("This is my message button."), dismissButton: .default(Text("Awesome!")))
+                }
+            }
+            
+            VStack {
+                Text("Second one, knock knock joke")
+                    .fontWeight(.semibold)
+                    .foregroundColor(Color.red)
+                Button(action: {
+                    print("Button pressed")
+                    self.whoIsVisible = true
+                }) {
+                    Text("Joke")
+                }
+                .alert(isPresented: $whoIsVisible) { () ->
+                    Alert in
+                    return Alert(title: Text("Who's there!"), message: Text("Cow. Bondi. Babe."), dismissButton: .default(Text("Eww!")))
+                }
+            }
+        }
+        
+        
     }
 }
 
